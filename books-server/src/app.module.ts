6e94785733c80f1863c882bcca42d528e7b36ef3
Reda Baka <rbaka79@gmail.com>
2023-09-18 19:46:31 +0200

    setup static serve, enable cors

--- books-server/src/app.module.ts
+++ books-server/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BookService } from './book/book.service';
 import { BookModule } from './book/book.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'build'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -15,6 +19,6 @@ import { ConfigModule } from '@nestjs/config';
     PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService, BookService],
+  providers: [BookService],
 })
 export class AppModule {}
